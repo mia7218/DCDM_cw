5d149a0cd81c76eff0deeeb0174f3bed52e6b4c9
maisiev <maisie.varcoe@yahoo.co.uk>
2025-11-14 15:12:29 +0000

    Update schema with data types and foreign key
    Updated data types and constraints for several tables, including adding character set to parameter_id and modifying the primary key type in Query_Genes. Cleaned up comments and removed unused insert statements. Added foreign key connection between Data and Parameter_Description tables.

--- DCDM_database.sql
+++ DCDM_database.sql
@@ -5,20 +5,22 @@ CREATE TABLE Data (
     gene_symbol varchar(30),
     mouse_strain varchar(5),
     mouse_life_stage varchar(17),
-    parameter_id varchar (20),
+    parameter_id VARCHAR(20) CHARACTER SET utf8mb4 NOT NULL,
     parameter_name varchar(74),
     pvalue varchar (20)
-); 
+);
+
 
 #create parameter description 
 CREATE TABLE Parameter_Description (
-    parameter_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    parameter_id varchar(20) CHARACTER SET utf8mb4 NOT NULL PRIMARY KEY,
     name varchar(100),
     description varchar(300),
     origin_id int(5)
-); 
+   
+);
 
-#create phenotype procedure table
+# create phenotype procedure table
 CREATE TABLE Phenotype_Procedure (
 
 	IMPC_parameter_origin_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -28,14 +30,14 @@ CREATE TABLE Phenotype_Procedure (
 ); 
 
 
-#create query genes table
+# create query genes table
 CREATE TABLE Query_Genes (
-	 gene_symbol int NOT NULL PRIMARY KEY AUTO_INCREMENT
+	 gene_symbol varchar NOT NULL PRIMARY KEY AUTO_INCREMENT
 	
-);
+); 
 
 #Creating "human disease" table
-create table Human_disease(
+create table Human_Disease(
 	gene_accession_id varchar(15) not null PRIMARY KEY,
 	DO_disease_id varchar(15),
 	DO_disease_name	varchar(70),
@@ -43,39 +45,8 @@ create table Human_disease(
 	); 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# put the first row of data into the Gene table
-INSERT INTO Gene (gene_symbol, name, cosmic_gene_id, chromosome, genome_start, genome_stop, molecular_genetics) 
-VALUES ("A1CF", "APOBEC1 complementation factor", "COSG46891", "10", 52559169, 52645435, "Dom"); 
-#second row of data
-INSERT INTO Gene (gene_symbol, name, cosmic_gene_id, chromosome, genome_start, genome_stop, molecular_genetics) 
-VALUES ("ABI1", "abl interactor 1",	"COSG5120",	"10",	27035522,	27150016,	"Dom"); 
-#third row
-INSERT INTO Gene (gene_symbol, name, cosmic_gene_id, chromosome, genome_start, genome_stop, molecular_genetics) 
-VALUES ("ABL1",	"ABL proto-oncogene 1, non-receptor tyrosine kinase",	"COSG4968",	"9", 133589333, 133763062,	"Rec"); 
-#fourth row
-INSERT INTO Gene (gene_symbol, name, cosmic_gene_id, chromosome, genome_start, genome_stop, molecular_genetics) 
-VALUES ("ABL2",	"ABL proto-oncogene 2, non-receptor tyrosine kinase",	"COSG36573",	"1",	179068462,	179198819,	"Dom");
-
-Select * from Gene;
-Select * from Gene where chromosome = 10;
-
-Select gene_id, gene_symbol, name, molecular_genetics from Gene where molecular_genetics = "Dom";
-Select gene_id, gene_symbol, name, molecular_genetics from Gene where genome_start > 27150016 and genome_start < 57150016;
-
-select distinct molecular_genetics from Gene; 
+ALTER TABLE Data
+ADD CONSTRAINT fk_data_parameter
+FOREIGN KEY (parameter_id)
+REFERENCES Parameter_Description(parameter_id);
 
